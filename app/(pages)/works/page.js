44d04fe42e3a4630801c48e968b304d303cc2329
Fabian <fabrofabian@gmail.com>
2025-01-19 00:47:09 -1000

    created works page

---
+++ app/(pages)/works/page.js
@@ -0,0 +1,28 @@
+import data from "../../data.json";
+
+export default function Works() {
+
+    
+
+    return ( 
+    <main>
+        <h2>Works</h2>
+        {/* <iframe style={{border: 0, width: "400px", height: "472px"}} src={"https://bandcamp.com/EmbeddedPlayer/album=2207066508/size=large/bgcol=333333/linkcol=4ec5ec/artwork=small/transparent=true/"} seamless><a href={"https://firahfabe.bandcamp.com/album/chill-times"}>Chill Times by FirahFabe</a></iframe> */}
+
+        <h3>Game Music</h3>
+        <iframe style={{border: 0, width: "400px", height: "400px"}} src="https://bandcamp.com/EmbeddedPlayer/album=57617531/size=large/bgcol=333333/linkcol=e32c14/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/demons-of-the-venta-quemada-game-soundtrack">Demons of the Venta Quemada Game Soundtrack by FirahFabe</a></iframe>
+
+        <iframe style={{border: 0, width: "400px", height: "400px"}} src="https://bandcamp.com/EmbeddedPlayer/album=1641999032/size=large/bgcol=333333/linkcol=9a64ff/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/the-mysterious-gate-game-soundtrack">The Mysterious Gate Game Soundtrack by FirahFabe</a></iframe>
+
+        <h3>Original Works</h3>
+        <iframe style={{border: 0, width: "100%", height: "120px"}} src="https://bandcamp.com/EmbeddedPlayer/album=2207066508/size=large/bgcol=333333/linkcol=0f91ff/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/chill-times">Chill Times by FirahFabe</a></iframe>
+
+        <h3>Game Music Covers</h3>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/33xvX9Pwy60?si=p9erkYiAzqS__wdE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        
+    </main>
+    )
+}
+
+
