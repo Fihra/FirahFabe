d88d86bdfbb1065ddec9959b0bbae38f9032a71b
Fabian <fabrofabian@gmail.com>
2025-01-19 00:46:57 -1000

    created services page

---
+++ app/(pages)/services/page.js
@@ -0,0 +1,18 @@
+import data from "../../data.json";
+
+export default function Services() {
+    return ( 
+    <main>
+        <h2>Services</h2>
+
+        <ul>
+            <li>Audio Implementation</li>
+            <li>Music</li>
+            <li>Sound Design</li>
+            <li>Rondalla Live instrument Recording</li>
+            <li>Kulintang Recording</li>
+            <li>Bass Guitar Recording</li>
+        </ul>
+    </main>
+    )
+}
