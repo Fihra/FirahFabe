711b3b13bc9f3e8c73b22247386e9120fb94ffbe
Fabian <fabrofabian@gmail.com>
2025-01-19 00:46:35 -1000

    created home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import styles from "./page.module.css";
+import data from "./data.json";
 
 export default function Home() {
+
   return (
-    <div >
+    <div>
       <main>
-        FirahFabe
+      {data.headerRoles.map((role, i) => <p key={i}>{role}</p>)}
       </main>
-      
+      <section>
+        <img src={data.headshot} width="200px" height="100%" style={{filter: "brightness(75%)"}}/>
+        <p>{data.intro[0]}</p>
+      </section>
     </div>
   );
 }
