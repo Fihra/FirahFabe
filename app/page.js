32e1a510fb1c7112ac7cbd9605407b91169d7604
Fabian <fabrofabian@gmail.com>
2025-01-20 00:04:59 -1000

    add mobile and desktop view for home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,25 @@
+'use client';
 import styles from "./page.module.css";
 import data from "./data.json";
+import { useMediaQuery } from "@mui/material";
+
 
 export default function Home() {
+  const mobileView = useMediaQuery('(min-width:800px)');
+
+  const showDesktopView = () => {
+    return data.headerRoles.map((role, i) => <span key={i}>{role}</span>)
+
+  }
+
+  const showMobileView = () => {
+    return data.headerRoles.map((role, i) => <p key={i}>{role}</p>)
+  }
 
   return (
     <div>
-      <main>
-      {data.headerRoles.map((role, i) => <p key={i}>{role}</p>)}
+      <main className={styles.headerRoles}>
+        {mobileView ? showDesktopView(): showMobileView()}
       </main>
       <section>
         <img src={data.headshot} width="200px" height="100%" style={{filter: "brightness(75%)"}}/>
