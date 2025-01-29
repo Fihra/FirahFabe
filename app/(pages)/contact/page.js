5cfea9155ed7148de3e612c1268505a7423d7633
Fabian <fabrofabian@gmail.com>
2025-01-28 23:29:50 -1000

    setup form that sends an email, is working

--- app/(pages)/contact/page.js
+++ app/(pages)/contact/page.js
@@ -1,50 +1,112 @@
 'use client';
 
-import data from "../../data.json";
-import { Box, TextField, Grid2} from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import { Box, TextField, Grid2, Button, useMediaQuery} from "@mui/material";
 import styles from "../../(styles)/layout.module.css";
-
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    useEffect(() => {
+        emailjs.init(process.env.PUBLIC_KEY);
+    }, []);
+
+    const mobileView = useMediaQuery('(max-width:800px)');
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({...formData,
+            [name]: value
+        });
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+
+        emailjs.send(
+            process.env.NEXT_PUBLIC_SERVICE_ID,
+            process.env.NEXT_PUBLIC_TEMPLATE_ID,
+            formData,
+            process.env.NEXT_PUBLIC_KEY
+        )
+        .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+
+    }
 
     return ( 
     <main>
         <h2>Contact</h2>
 
-        
+        <form onSubmit={handleSubmit}>
         <Box className={styles.contactContainer}>
-            <Grid2 container spacing={5}>
+            <Grid2 container spacing={5} direction="column">
+            
             <TextField
                 className={styles.contactTextField}
-                sx={{width: "30%", margin: "0 auto"}}
+                name="name"
+                sx={{width: mobileView ? "100%" : "50%", margin: "0 auto", input: {color: "white"}}}
                 required
-                id="filled-require"
+                id="name-form"
                 label="Name"
                 variant="standard"
+                onChange={handleChange}
             />
 
             <TextField
                 className={styles.contactTextField}
-                sx={{width: "20%", margin: "0 auto"}}
+                name="email"
+                sx={{width: mobileView ? "100%" : "50%", margin: "0 auto", input: {color: "white"}}}
                 required
-                id="filled-req"
+                id="email-form"
                 label="Email"
                 variant="standard"
+                type="email"
+                onChange={handleChange}
             />
 
             <TextField
                 className={styles.contactTextField}
-                sx={{width: "80%", margin: "0 auto"}}
+                name="message"
+                sx={{width: mobileView ? "100%" : "50%", margin: "0 auto"}}
+                slotProps={{
+
+                }}
                 required
-                id="outlined-multiline"
+                id="message-form"
                 label="Message"
                 multiline
                 maxRows={4}
+                onChange={handleChange}
 
             />
-
+            <Button
+                className={styles.submitButton}
+                variant="outlined"
+                pill="true"
+                sx={{margin: "0 auto", color: "#CAFFB9", borderColor: "black"}}
+                type="submit"
+            >
+                Submit
+            </Button>
+            
+            
             </Grid2>
         </Box>
+        </form>
 
     </main>
     )
