fad0cd5532097f54c1088dd2fbe896f62e187352
Fabian <fabrofabian@gmail.com>
2025-01-30 15:20:22 -1000

    added reels and organized spacings within each works section

--- app/(pages)/works/page.js
+++ app/(pages)/works/page.js
@@ -1,42 +1,68 @@
 'use client';
 import { useMediaQuery } from "@mui/material";
+import data from "../../data.json";
+import styles from "../../page.module.css";
 
 export default function Works() {
     const mobileView = useMediaQuery('(max-width:800px)');
 
+    const { gamemusic, gamesound } = data.works;
+
+    const showTimeStamps = (audioData) => {
+        const timeStamps = audioData.map((game, i) => {
+            return (
+                <div key={i} style={{marginBottom: 12}}>
+                    <p>{game.timestamp} <a href={game.gamelink} target={game.gamelink !== "#" ? "_blank": "_parent"}><b>{game.title}</b></a></p>
+                </div>
+            )
+        })
+        return timeStamps;
+    }
+    
     return ( 
     <main>
         <h2>Works</h2>
-        <h3>Game Music</h3>
-        {
-            mobileView ?
-            <iframe style={{border: 0, width: "100%", height: "42px"}} src="https://bandcamp.com/EmbeddedPlayer/album=57617531/size=small/bgcol=333333/linkcol=e32c14/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/demons-of-the-venta-quemada-game-soundtrack">Demons of the Venta Quemada Game Soundtrack by FirahFabe</a></iframe>
-            :
-            <iframe style={{border: 0, width: "400px", height: "400px"}} src="https://bandcamp.com/EmbeddedPlayer/album=57617531/size=large/bgcol=333333/linkcol=e32c14/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/demons-of-the-venta-quemada-game-soundtrack">Demons of the Venta Quemada Game Soundtrack by FirahFabe</a></iframe>
-        }
+        <section className={styles.worksSection}>
+            <h3>Game Music</h3>
+            {
+                mobileView ?
+                <iframe width="100%" height="200" src="https://www.youtube.com/embed/bLd8mvlZ5Q8?si=dWdUvn-aOHAwgbKw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                :
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/bLd8mvlZ5Q8?si=dWdUvn-aOHAwgbKw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            }
+            {showTimeStamps(gamemusic)}
+        </section>
 
-        {
-            mobileView ?
-            <iframe style={{border: 0, width: "100%", height: "42px"}} src="https://bandcamp.com/EmbeddedPlayer/album=1641999032/size=small/bgcol=333333/linkcol=9a64ff/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/the-mysterious-gate-game-soundtrack">The Mysterious Gate Game Soundtrack by FirahFabe</a></iframe>
-            :
-            <iframe style={{border: 0, width: "400px", height: "400px"}} src="https://bandcamp.com/EmbeddedPlayer/album=1641999032/size=large/bgcol=333333/linkcol=9a64ff/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/the-mysterious-gate-game-soundtrack">The Mysterious Gate Game Soundtrack by FirahFabe</a></iframe>
-        }
+        <section className={styles.worksSection}>
+            <h3>Sound Design</h3>
+            {
+                mobileView ?
+                <iframe width="100%" height="200" src="https://www.youtube.com/embed/DY0kA6-T2BY?si=0JxrwFxEu9Pb8SWa" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                :
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/DY0kA6-T2BY?si=0JxrwFxEu9Pb8SWa" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            }
+            {showTimeStamps(gamesound)}
+        </section>
 
+        <section className={styles.worksSection}>
         <h3>Original Works</h3>
         {
             mobileView ?
             <iframe style={{border: 0, width: "100%", height: "42px"}} src="https://bandcamp.com/EmbeddedPlayer/album=2207066508/size=small/bgcol=333333/linkcol=4ec5ec/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/chill-times">Chill Times by FirahFabe</a></iframe>
             :
-            <iframe style={{border: 0, width: "100%", height: "120px"}} src="https://bandcamp.com/EmbeddedPlayer/album=2207066508/size=large/bgcol=333333/linkcol=0f91ff/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/chill-times">Chill Times by FirahFabe</a></iframe>
+            <iframe style={{border: 0, width: "400px", height: "472px"}} src="https://bandcamp.com/EmbeddedPlayer/album=2207066508/size=large/bgcol=333333/linkcol=4ec5ec/artwork=small/transparent=true/" seamless><a href="https://firahfabe.bandcamp.com/album/chill-times">Chill Times by FirahFabe</a></iframe>
         }
+        </section>
         
+        <section className={styles.worksSection}>
         <h3>Game Music Covers</h3>
         {
             mobileView ?
-            <iframe width="100%" height="200" src="https://www.youtube.com/embed/33xvX9Pwy60?si=p9erkYiAzqS__wdE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="100%" height="200" src="https://www.youtube.com/embed/33xvX9Pwy60?si=p9erkYiAzqS__wdE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             :
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/33xvX9Pwy60?si=p9erkYiAzqS__wdE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/33xvX9Pwy60?si=p9erkYiAzqS__wdE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         }
+        </section>
     </main>
     )
 }
