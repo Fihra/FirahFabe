5770ff2b6beccb8e6473277a238f447c81a38ee2
Fabian <fabrofabian@gmail.com>
2025-01-30 20:25:35 -1000

    added mobile view for about section

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -1,16 +1,36 @@
+'use client';
+
 import data from "../../data.json";
 import styles from "../../(styles)/layout.module.css";
+import { Grid2, Box } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 export default function About() {
+    const mobileView = useMediaQuery('(max-width:800px)');
+
     const { about, musicStyle, laudImage, laudAndKulintang } =  data;
     return ( 
     <main className={styles.aboutSection}>
-        <h2>About</h2>
-        <p>{about}</p>
-        <img src={laudImage} height={300} width={400} style={{float: "right"}}/>
-        <br/>
-        <p>{musicStyle}</p>
-        <img src={laudAndKulintang} height={500} width={300}/>
+        <h2 style={{paddingBottom: 12}}>About</h2>
+        <Box sx={{width: '100%'}}>
+            <Grid2 container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
+                <Grid2 size={{sm: 2, md: 6}}>
+                    <p>{about}</p>
+                </Grid2>
+                <Grid2 size={{sm: 2, md: 6}}>
+                    <img className={styles.aboutImages} src={laudImage} height={300} width={400}/>
+                </Grid2>
+            </Grid2>
+            <br/>
+            <Grid2 container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
+                <Grid2 size={{sm: 2, md: 6}} style={{order: mobileView ? 2 : 1}}>
+                    <img className={styles.aboutImages} src={laudAndKulintang} height={500} width={mobileView ? "100%" : 300}/>
+                </Grid2>
+                <Grid2 size={{sm: 2, md: 6}} style={{order: mobileView ? 1 : 2}}>
+                    <p>{musicStyle}</p>
+                </Grid2>
+            </Grid2>
+        </Box>
     </main>
     )
 }
