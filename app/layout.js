25af6d92d1eb39da57e50a6e09d74fa263aa8209
Fabian <fabrofabian@gmail.com>
2025-01-30 15:19:31 -1000

    spacing for main body

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${styles.mainBody}`}>
         <AppRouterCacheProvider>
         <header className={styles.topHeader}><SocialMediaLinks/><Navbar/></header>
         
